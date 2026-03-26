2670268f95c6088e3eecaaa59845a86f08ac63db
nrosanti-datascience <nicollerosanti@outlook.com>
2026-03-25 23:32:20 -0300

    editei para incluir o código

--- src/br/com/fiapride/main/SistemaPrincipal.java
+++ src/br/com/fiapride/main/SistemaPrincipal.java
@@ -1,5 +1,27 @@
 package br.com.fiapride.main;
 
-public class SistemaPrincipal {
+import br.com.fiapride.model.Veiculos;
 
+public class SistemaPrincipal {
+    
+    public static void main(String[] args) {
+        
+        // Instancia o objeto
+        Veiculos carro1 = new Veiculos();
+        
+        // Usando SETTERS (não pode mais acessar direto!)
+        carro1.setDono("Carlos");
+        carro1.setPlaca("ABC-1234");
+
+        // Abastecendo o carro
+        carro1.adicionarGasolina(50);
+        carro1.adicionarGasolina(100);
+
+        // Exibindo os dados com GETTERS
+        System.out.println(
+            "Dono: " + carro1.getDono() +
+            " | Placa: " + carro1.getPlaca() +
+            " | Gasolina: " + carro1.getGasolina()
+        );
+    }
 }
--- src/br/com/fiapride/model/Veiculo.java
+++ src/br/com/fiapride/model/Veiculo.java
@@ -1,5 +1,45 @@
 package br.com.fiapride.model;
 
-public class Veiculo {
+public class Veiculos {
 
+    private String dono;
+    private String placa;
+    private int gasolina;
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public int getGasolina() {
+        return gasolina;
+    }
+
+
+    public void adicionarGasolina(int litro) {
+        if (litro > 0) {
+            this.gasolina += litro;
+        } else {
+            System.out.println("Valor inválido para abastecimento!");
+        }
+    }
+
+    public void gastoGasolina(int litro) {
+        if (litro > 0 && litro <= this.gasolina) {
+            this.gasolina -= litro;
+        } else {
+            System.out.println("Consumo inválido!");
+        }
+    }
 }
